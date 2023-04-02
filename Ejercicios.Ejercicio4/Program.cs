0fa942c7efed991dd82d8a837c7e8911deffe7ce
GabyFCE <duartegabriel2017@gmail.com>
2023-04-01 23:06:27 -0300

    Add project files.

---
+++ Ejercicios.Ejercicio4/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+while (true)
+{
+    Console.WriteLine("Ingrese una frase, si ingresa 'fin' el programa finalizará");
+    string frase = Console.ReadLine();
+    if (frase == "fin")
+    {
+        Console.WriteLine("Finalizando programa...");
+        break;
+    }
+}
